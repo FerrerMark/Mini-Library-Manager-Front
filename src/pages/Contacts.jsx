e3f624835663df1f16817afc27fe16c7f82aa496
FerrerMark <ferrerjohnmark26@gmail.com>
2025-07-19 10:26:20 +0800

    add documentation in about page

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -9,11 +9,9 @@ const Contacts = () => {
             <h1 className="h1">Contacts</h1>
             <div className="contact body">
                 <h2>Links</h2>
-                <p><a href="https://github.com/FerrerMark" target="_blank" rel="noopener noreferrer">GitHub</a></p>
+                <p><a href="https://github.com/FerrerMark/Mini-Library-Manager-Back" target="_blank" rel="noopener noreferrer">GitHub</a></p>
 
                 <p><a href="https://portfolio-ferrerjohnmark26-gmailcoms-projects.vercel.app/" target="_blank" rel="noopener noreferrer">Personal Portfolio</a></p>
-                
-                
             </div>
             <AddBtn />
         </div>
